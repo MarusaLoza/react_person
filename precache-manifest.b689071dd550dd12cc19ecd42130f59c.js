f3605895b7678d7f8c4efe9696a01d0cf73caaf9
Loza Marusa <marusa.loza@gmail.com>
2021-10-11 13:02:40 +0300

    Updates

---
+++ precache-manifest.b689071dd550dd12cc19ecd42130f59c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8313d8b3db57580b498a64bea417b4aa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "37d46f06dd2e23ad915f",
+    "url": "./static/css/main.5c421051.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d46f06dd2e23ad915f",
+    "url": "./static/js/main.95a31f00.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
